ccff9c6d1c18b8b0b23bc3841be1f28ac99c0bc9
CoolerVoid <coolerlair@gmail.com>
2018-01-03 01:36:36 -0200

    fix code style in VIM...  nodepad++  crack the lines :-(

--- server/Firefox_Tunnel_win32/src/utils.cpp
+++ server/Firefox_Tunnel_win32/src/utils.cpp
@@ -20,143 +20,142 @@ int Get_CMD_result(
     string& ListStdErr, //Return List of StdErr
     uint32_t& RetCode)    //Return Exit Code
 {
-    int                  Success;
-    SECURITY_ATTRIBUTES  security_attributes;
-    HANDLE               stdout_rd = INVALID_HANDLE_VALUE;
-    HANDLE               stdout_wr = INVALID_HANDLE_VALUE;
-    HANDLE               stderr_rd = INVALID_HANDLE_VALUE;
-    HANDLE               stderr_wr = INVALID_HANDLE_VALUE;
-    PROCESS_INFORMATION  process_info;
-    STARTUPINFO          startup_info;
-    thread               stdout_thread;
-    thread               stderr_thread;
+	int                  Success;
+	SECURITY_ATTRIBUTES  security_attributes;
+	HANDLE               stdout_rd = INVALID_HANDLE_VALUE;
+    	HANDLE               stdout_wr = INVALID_HANDLE_VALUE;
+    	HANDLE               stderr_rd = INVALID_HANDLE_VALUE;
+    	HANDLE               stderr_wr = INVALID_HANDLE_VALUE;
+	PROCESS_INFORMATION  process_info;
+    	STARTUPINFO          startup_info;
+    	thread               stdout_thread;
+    	thread               stderr_thread;
 
-    security_attributes.nLength              = sizeof(SECURITY_ATTRIBUTES);
-    security_attributes.bInheritHandle       = TRUE;
-    security_attributes.lpSecurityDescriptor = nullptr;
+    	security_attributes.nLength              = sizeof(SECURITY_ATTRIBUTES);
+    	security_attributes.bInheritHandle       = TRUE;
+    	security_attributes.lpSecurityDescriptor = nullptr;
 
-    if (!CreatePipe(&stdout_rd, &stdout_wr, &security_attributes, 0) || !SetHandleInformation(stdout_rd, HANDLE_FLAG_INHERIT, 0)) 
+    	if(!CreatePipe(&stdout_rd, &stdout_wr, &security_attributes, 0) || !SetHandleInformation(stdout_rd, HANDLE_FLAG_INHERIT, 0)) 
 		return -1;
     
 
-    if (!CreatePipe(&stderr_rd, &stderr_wr, &security_attributes, 0) || !SetHandleInformation(stderr_rd, HANDLE_FLAG_INHERIT, 0)) 
+    	if(!CreatePipe(&stderr_rd, &stderr_wr, &security_attributes, 0) || !SetHandleInformation(stderr_rd, HANDLE_FLAG_INHERIT, 0)) 
 	{
-        if (stdout_rd != INVALID_HANDLE_VALUE) CloseHandle(stdout_rd);
-        if (stdout_wr != INVALID_HANDLE_VALUE) CloseHandle(stdout_wr);
-        return -2;
-    }
+        	if (stdout_rd != INVALID_HANDLE_VALUE) CloseHandle(stdout_rd);
+        	if (stdout_wr != INVALID_HANDLE_VALUE) CloseHandle(stdout_wr);
+        		return -2;
+    	}
 
-    ZeroMemory(&process_info, sizeof(PROCESS_INFORMATION));
-    ZeroMemory(&startup_info, sizeof(STARTUPINFO));
+    	ZeroMemory(&process_info, sizeof(PROCESS_INFORMATION));
+    	ZeroMemory(&startup_info, sizeof(STARTUPINFO));
 
-    startup_info.cb         = sizeof(STARTUPINFO);
-    startup_info.hStdInput  = 0;
-    startup_info.hStdOutput = stdout_wr;
-    startup_info.hStdError  = stderr_wr;
+    	startup_info.cb         = sizeof(STARTUPINFO);
+    	startup_info.hStdInput  = 0;
+    	startup_info.hStdOutput = stdout_wr;
+    	startup_info.hStdError  = stderr_wr;
 
-    if(stdout_rd || stderr_rd)
+    	if(stdout_rd || stderr_rd)
 		startup_info.dwFlags |= STARTF_USESTDHANDLES;
 
     // Make a copy because CreateProcess needs to modify string buffer
-    char CmdLineStr[MAX_PATH];
-    strncpy(CmdLineStr, CmdLine.c_str(), MAX_PATH);
-    CmdLineStr[MAX_PATH-1] = 0;
+    	char CmdLineStr[MAX_PATH];
+    	strncpy(CmdLineStr, CmdLine.c_str(), MAX_PATH);
+    	CmdLineStr[MAX_PATH-1] = 0;
 
-    Success = CreateProcess(
-        nullptr,
-        CmdLineStr,
-        nullptr,
-        nullptr,
-        TRUE,
-        0,
-        nullptr,
-        CmdRunDir.c_str(),
-        &startup_info,
-        &process_info
-    );
-    CloseHandle(stdout_wr);
-    CloseHandle(stderr_wr);
+    	Success = CreateProcess(
+        	nullptr,
+        	CmdLineStr,
+        	nullptr,
+        	nullptr,
+        	TRUE,
+        	0,
+        	nullptr,
+        	CmdRunDir.c_str(),
+        	&startup_info,
+        	&process_info
+    	);
+    	CloseHandle(stdout_wr);
+    	CloseHandle(stderr_wr);
 
-    if(!Success) 
+    	if(!Success) 
 	{
-        CloseHandle(process_info.hProcess);
-        CloseHandle(process_info.hThread);
-        CloseHandle(stdout_rd);
-        CloseHandle(stderr_rd);
-        return -4;
-    }
-    else {
-        CloseHandle(process_info.hThread);
-    }
+        	CloseHandle(process_info.hProcess);
+        	CloseHandle(process_info.hThread);
+        	CloseHandle(stdout_rd);
+        	CloseHandle(stderr_rd);
+        	return -4;
+    	}else {
+        	CloseHandle(process_info.hThread);
+    	}
 
-    if(stdout_rd) 
+    	if(stdout_rd) 
 	{
-        stdout_thread=thread([&]() 
+        	stdout_thread=thread([&]() 
 		{
-            DWORD  n;
-            const size_t bufsize = 1000;
-            char buffer [bufsize];
+            		DWORD  n;
+            		const size_t bufsize = 1000;
+            		char buffer [bufsize];
 			
-            while(1) 
+            		while(1) 
 			{
-                n = 0;
-                int Success = ReadFile(
-                    stdout_rd,
-                    buffer,
-                    (DWORD)bufsize,
-                    &n,
-                    nullptr
-                );
+                		n = 0;
+                		int Success = ReadFile(
+                    			stdout_rd,
+                    			buffer,
+                    			(DWORD)bufsize,
+                    			&n,
+                    			nullptr
+                		);
 
-                if(!Success || n == 0)
-                    break;
-                string s(buffer, n);
-                ListStdOut += s;
-            }
-        });
-    }
+                		if(!Success || n == 0)
+                    			break;
+                		string s(buffer, n);
+                		ListStdOut += s;
+            		}
+        	});
+    	}
 
-    if(stderr_rd) 
+    	if(stderr_rd) 
 	{
-        stderr_thread=thread([&]() 
+        	stderr_thread=thread([&]() 
 		{
-            DWORD        n;
-            const size_t bufsize = 1000;
-            char         buffer [bufsize];
-            while(1) 
+           	 	DWORD n;
+            		const size_t bufsize = 1000;
+            		char buffer [bufsize];
+            		while(1) 
 			{
-                n = 0;
-                int Success = ReadFile(
-                    stderr_rd,
-                    buffer,
-                    (DWORD)bufsize,
-                    &n,
-                    nullptr
-                );
-                if(!Success || n == 0)
-                    break;
-                string s(buffer, n);
-                ListStdOut += s;
-            }
-        });
-    }
+                		n = 0;
+                		int Success = ReadFile(
+                    			stderr_rd,
+                    			buffer,
+                    			(DWORD)bufsize,
+                    			&n,
+                    			nullptr
+                		);
+                		if(!Success || n == 0)
+                    			break;
+                		string s(buffer, n);
+                		ListStdOut += s;
+            		}
+        	});
+    	}
 
-    WaitForSingleObject(process_info.hProcess,    INFINITE);
-    if(!GetExitCodeProcess(process_info.hProcess, (DWORD*) &RetCode))
-        RetCode = -1;
+    	WaitForSingleObject(process_info.hProcess,    INFINITE);
+    	if(!GetExitCodeProcess(process_info.hProcess, (DWORD*) &RetCode))
+        	RetCode = -1;
 
-    CloseHandle(process_info.hProcess);
+    	CloseHandle(process_info.hProcess);
 
-    if(stdout_thread.joinable())
-        stdout_thread.join();
+    	if(stdout_thread.joinable())
+        	stdout_thread.join();
 
-    if(stderr_thread.joinable())
-        stderr_thread.join();
+    	if(stderr_thread.joinable())
+        	stderr_thread.join();
 
-    CloseHandle(stdout_rd);
-    CloseHandle(stderr_rd);
+    	CloseHandle(stdout_rd);
+    	CloseHandle(stderr_rd);
 
-    return 0;
+    	return 0;
 }
  
 static inline bool is_base64(unsigned char z) 
@@ -263,22 +262,23 @@ string readfile(const string &filepath)
 string exec_command(string cmd) 
 {
 	int rc=0;
-    uint32_t RetCode=0;
-    string ListStdOut;
-    string ListStdErr;
+    	uint32_t RetCode=0;
+    	string ListStdOut;
+    	string ListStdErr;
 	string pipe_tmp="C:\\Windows\\System32\\cmd.exe /c "+cmd;
 	
-    rc = Get_CMD_result(
-        pipe_tmp,    //Command Line
-        ".",                                     //CmdRunDir
-        ListStdOut,                              //Return List of StdOut
-        ListStdErr,                              //Return List of StdErr
-        RetCode                                  //Return Exit Code
-    );
-    if(rc < 0) 
+    	rc = Get_CMD_result(
+        	pipe_tmp,    //Command Line
+        	".",                                     //CmdRunDir
+        	ListStdOut,                              //Return List of StdOut
+        	ListStdErr,                              //Return List of StdErr
+        	RetCode                                  //Return Exit Code
+    	);
+
+    	if(rc < 0) 
 		cout << "ERROR: Get_CMD_result() function\n";
     
-    return ListStdOut;
+    	return ListStdOut;
 }
  
 string get_windows_username()
